be724eca6ea8514c86f0cbb5c89831d94bd3a930
Mateusz Iwaniuk <mateusz.iwaniuk11@gmail.com>
2019-09-10 23:10:12 +0200

    done home section

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('.nav')
+const navCloser = document.querySelector('.closer')
+
+const burger = document.querySelector('.burger')
+const lastLine = document.querySelector('.burger__line--last')
+
+navCloser.addEventListener('click', () => {
+    navCloser.classList.remove('on')
+    nav.classList.remove('on');
+    lastLine.classList.remove('on')
+})
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('on')
+    lastLine.classList.toggle('on')
+    navCloser.classList.toggle('on')
+})
