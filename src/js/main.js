f5a5246a09d3d08fde3b4578f4dd0d6c13ec8db4
Iwaniukooo11 <mateusz.iwaniuk11@gmail.com>
2020-01-15 17:47:13 +0100

    change description i nabout

--- src/js/main.js
+++ src/js/main.js
@@ -1,58 +1,71 @@
-const nav = document.querySelector('.nav')
-const navLinks = document.querySelectorAll('.nav__link')
-const navCloser = document.querySelector('.closer')
-const rocket = document.querySelector('.rocket')
-const time = 800
-const burger = document.querySelector('.burger')
-const lastLine = document.querySelector('.burger__line--last')
-const allLines = document.querySelectorAll('.burger__line')
+const nav = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav__link");
+const navCloser = document.querySelector(".closer");
+const rocket = document.querySelector(".rocket");
+const time = 800;
+const burger = document.querySelector(".burger");
+const lastLine = document.querySelector(".burger__line--last");
+const allLines = document.querySelectorAll(".burger__line");
+// const VanillaScrollspy = require("vanillajs-scrollspy");
+import VanillaScrollspy from "vanillajs-scrollspy";
 
-allLines.forEach(element => window.addEventListener('scroll', () =>
-    element.getBoundingClientRect().top + window.pageYOffset >= window.outerHeight
-        ? element.classList.add('outMain')
-        : element.classList.remove('outMain')))
+const homePage = document.querySelector(".section.home");
+const projectGallery = document.querySelector(".project__gallery");
 
-navLinks.forEach(e => e.addEventListener('click', () => {
+allLines.forEach(element =>
+  window.addEventListener("scroll", () =>
+    element.getBoundingClientRect().top + window.pageYOffset >=
+    homePage.clientHeight
+      ? element.classList.add("outMain")
+      : element.classList.remove("outMain")
+  )
+);
+
+navLinks.forEach(e =>
+  e.addEventListener("click", () => {
     setTimeout(() => {
-        nav.classList.remove('on')
-        navCloser.classList.remove('on')
-        lastLine.classList.remove('on')
-    }, time - 10)
+      nav.classList.remove("on");
+      navCloser.classList.remove("on");
+      lastLine.classList.remove("on");
+    }, time - 10);
+  })
+);
 
-}))
+navCloser.addEventListener("click", () => {
+  navCloser.classList.remove("on");
+  nav.classList.remove("on");
+  lastLine.classList.remove("on");
+});
 
-navCloser.addEventListener('click', () => {
-    navCloser.classList.remove('on')
-    nav.classList.remove('on');
-    lastLine.classList.remove('on')
-})
+burger.addEventListener("click", () => {
+  nav.classList.toggle("on");
+  lastLine.classList.toggle("on");
+  navCloser.classList.toggle("on");
+});
 
-burger.addEventListener('click', () => {
-    nav.classList.toggle('on')
-    lastLine.classList.toggle('on')
-    navCloser.classList.toggle('on')
-})
+window.addEventListener(
+  "scroll",
+  () =>
+    (rocket.style.transform = `translateX(41%) translateY(-${window.pageYOffset /
+      4}%)`)
+);
 
-window.addEventListener('scroll', () => rocket.style.transform = `translateX(41%) translateY(-${window.pageYOffset / 4}%)`)
-
-import VanillaScrollspy from 'vanillajs-scrollspy';
-// import { all } from 'q'
-
-const navbar = document.querySelector('.nav');
+const navbar = document.querySelector(".nav");
 
 const scrollspy = new VanillaScrollspy(navbar, time);
 scrollspy.init();
 
-const sections = document.querySelectorAll('.section')
-sections.forEach((element, index) => index === 0 ? null : element.setAttribute('data-aos', 'fade-in'))
+const sections = document.querySelectorAll(".section");
+sections.forEach((element, index) =>
+  index === 0 ? null : element.setAttribute("data-aos", "fade-in")
+);
 
-const projectGallery = document.querySelector('.project__gallery')
 for (let i = 1; i < 7; i++) {
-    const link = document.createElement('a')
-    link.classList.add('project__link')
-    link.setAttribute('href', `assets/rocket_photos/${i}_max.jpg`)
-    link.innerHTML = `
+  const link = document.createElement("a");
+  link.classList.add("project__link");
+  link.setAttribute("href", `assets/rocket_photos/${i}_max.jpg`);
+  link.innerHTML = `
     <img class="project__img" src="assets/rocket_photos/${i}_min.jpg" />
-`
-    projectGallery.appendChild(link)
+`;
+  projectGallery.appendChild(link);
 }
